e3b937698fa6e6919750556ce17df861c6d9eba8
Jacob Casper <jacob.thomas.casper@gmail.com>
2019-01-31 16:03:27 -0600

    Created types.PlayingContainer

--- types/playing.go
+++ types/playing.go
@@ -5,12 +5,36 @@ import (
 )
 
 // Wraps spotify.CurrentlyPlaying for use by our ORM.
-type Playing struct {
-	AccountID spotify.ID
-	TrackID   spotify.ID
-	Timestamp int64
-}
+type (
+	Playing struct {
+		AccountID spotify.ID
+		TrackID   spotify.ID
+		Timestamp int64
+	}
+	PlayingContainer struct {
+		Plays []*Playing
+	}
+)
 
 func (p *Playing) Table() string {
 	return "PLAYING"
+}
+
+func (pc *PlayingContainer) MostPlayed() *Playing {
+	var maxCount int
+	var maxPlay *Playing
+	playCounts := make(map[spotify.ID]int)
+	for _, p := range pc.Plays {
+		if cnt, ok := playCounts[p.TrackID]; ok {
+			cnt += 1
+			playCounts[p.TrackID] = cnt
+			if cnt > maxCount {
+				maxPlay = p
+				maxCount = cnt
+			}
+		} else {
+			playCounts[p.TrackID] = 1
+		}
+	}
+	return maxPlay
 }
