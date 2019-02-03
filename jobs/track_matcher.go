6690ef6f304d184e54306fb478416bf06392e7c9
Jacob Casper <jacob.thomas.casper@gmail.com>
2019-02-03 05:40:01 -0600

    Destroy orm in track_matcher.go

--- jobs/track_matcher.go
+++ jobs/track_matcher.go
@@ -16,6 +16,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to create orm: ", err)
 	}
+	defer o.Destroy()
 
 	config := &clientcredentials.Config{
 		ClientID:     os.Getenv("SPOTIFY_ID"),
